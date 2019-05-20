bb2267cd27e6b7a773d5a6c3940e61b96697e46b
Draffaco <diegoadaco@gmail.com>
2019-05-20 16:37:00 +0200

    android c'est plus style, équilibrage toujours pas ok

--- app/src/main/java/com/example/androchess/UserBaseSQLite.java
+++ app/src/main/java/com/example/androchess/UserBaseSQLite.java
@@ -40,7 +40,7 @@ public class UserBaseSQLite extends SQLiteOpenHelper {
                 COL_UNIT1+", "+COL_UNIT2+", "+COL_UNIT3+", "+
                 COL_UNIT4+", "+COL_UNIT5+", "+COL_UNIT6+", "+
                 COL_UNIT7+", "+COL_UNIT8+", "+COL_UNIT9+") VALUES ('"+
-                "player1"+"', '"+"player1"+"', '"+
+                "Android1"+"', '"+"Android1"+"', '"+
                 "0"+"', '"+"0"+"', '"+
                 "Aircraft Damage"+"', '"+ "Aircraft Tank"+"', '"+"Aircraft Speed"+"', '"+
                 "Shooter Damage"+"', '"+"Shooter Tank"+"', '"+"Shooter Speed"+"', '"+
@@ -50,7 +50,7 @@ public class UserBaseSQLite extends SQLiteOpenHelper {
                 COL_UNIT1+", "+COL_UNIT2+", "+COL_UNIT3+", "+
                 COL_UNIT4+", "+COL_UNIT5+", "+COL_UNIT6+", "+
                 COL_UNIT7+", "+COL_UNIT8+", "+COL_UNIT9+") VALUES ('"+
-                "player2"+"', '"+"player2"+"', '"+
+                "Android2"+"', '"+"Android2"+"', '"+
                 "0"+"', '"+"0"+"', '"+
                 "Aircraft Damage"+"', '"+ "Aircraft Tank"+"', '"+"Aircraft Speed"+"', '"+
                 "Shooter Damage"+"', '"+"Shooter Tank"+"', '"+"Shooter Speed"+"', '"+
